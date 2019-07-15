f01776c9481f32045e16000bee045e85fed0ddcf
tranxuanduc1501 <52705263+tranxuanduc1501@users.noreply.github.com>
2019-07-15 22:00:46 +0700

    Add files via upload

---
+++ Bài 2.py
@@ -0,0 +1,6 @@
+a=2
+b=100
+Tổng=a+b
+print('Vậy tổng là', Tổng)
+Trungbìnhcộng= int((a+b))/2
+print('Vậy trung bình cộng là', Trungbìnhcộng)
---
+++ Bài 4.py
@@ -0,0 +1,14 @@
+x= float(input('Hãy nhập số đầu tiên:'))
+y= float(input('Hãy nhập số thứ hai:'))
+Tổng=x+y
+Tích=x*y
+Lũythừa=x**y
+Thương=x/y
+Hiệu=x-y
+Chialấydư=x%y
+print("Vậy tích 2 số là", Tích)
+print('vậy tổng 2 số là' ,Tổng)
+print("Vậy lũy thừa 2 số là", Lũythừa)
+print('Vậy thương 2 số là', Thương)
+print('Vậy hiệu 2 số là', Hiệu)
+print('Vậy số dư của 2 số là',Chialấydư)
