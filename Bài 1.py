cab6e19d1a287302c353e2303396b538125d39cc
tranxuanduc1501 <52705263+tranxuanduc1501@users.noreply.github.com>
2019-07-15 19:43:34 +0700

    Add files via upload

---
+++ Bài 1.py
@@ -0,0 +1,7 @@
+a=3
+b=2
+Tổng=a+b
+Hiệu=a-b
+Tích=a*b
+Thương=a/b
+print(Tổng, Hiệu, Tích, Thương)
---
+++ Bài 3.py
@@ -0,0 +1,8 @@
+x= int(input('Nhập số nguyên x:'))
+y= int(input('Nhập số nguyên y:'))
+p=x*y
+print('Vậy kết quả của p là', p)
+s=x+y
+print('Vậy kết quả của s là', s)
+q=2*s+p*(s-x)*(p+y)
+print('Vậy kết quả của q là', q)
